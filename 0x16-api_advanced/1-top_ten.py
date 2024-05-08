d53df2f4bbb6831b34b90af531a62c34751800bb
idah-github <guantaiidah@gmail.com>
2024-05-08 06:50:47 +0000

    debug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-'''
-displays top 10 hot posts
-'''
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    queries the Reddit API and prints the titles of
-    the first 10 hot posts listed for a given subreddit.
-    """
-    user_agent = {'User-agent': 'metrine'}
-    sub = requests.get('http://www.reddit.com/r/{}/hot.json'
-                       .format(subreddit), headers=user_agent)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
 
-    try:
-        sub = sub.json().get('data')
-        sub = sub.get('children')
-        i = 0
-        for obj in sub:
-            if i > 9:
-                break
-            print(obj['data'].get('title'))
-            i += 1
-
-    except Exception as e:
-        print('None')
